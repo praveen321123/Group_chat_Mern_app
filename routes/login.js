a7851720b2a1fb00985cf46960810c71d90d7c17
Praveen S <praveens321123@gmail.com>
2024-09-08 13:06:15 +0530

    styling changes!

--- routes/login.js
+++ routes/login.js
@@ -1,18 +1,14 @@
-const { log } = require("console");
 const express = require("express")
 const router = express.Router()
 const path = require("path")
 
 router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'login.html'));
-  console.log(__dirname, "adjasdkjb");
-  
 });
 
 router.post('/login', (req, res) => {
   const username = req.body.username;
   if (username) {
-    console.log(username);
     res.redirect(`/?username=${username}`);
   } else {
     res.send('Username is required');
